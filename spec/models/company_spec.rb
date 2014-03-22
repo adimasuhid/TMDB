3a0a4c79cfef2b9d4b4288e31f30fb88a1611ece
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-03-22 11:33:23 +0800

    Adds Search to Tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -12,4 +12,33 @@ describe Company do
       expect(subject).to validate_uniqueness_of(:company).case_insensitive
     end
   end
+
+  describe ".search" do
+    context "with approved companies" do
+      before :each do
+        company.approved = true
+        company.save!
+      end
+
+      it "returns a collection" do
+        expect(Company.search("mystring")).to be_an_instance_of ActiveRecord::Relation
+      end
+
+      it "returns a sample with instances of Company" do
+        expect(Company.search("mystring").sample).to be_an_instance_of Company
+      end
+
+      it "returns a sample that contains the search term as company attribute" do
+        search_term = "mystring"
+        expect(Company.search(search_term).sample.company.downcase).to eq(search_term)
+      end
+    end
+
+    context "with unapproved companies" do
+      it "returns a blank object" do
+        company.save
+        expect(Company.search("mystring")).to be_blank
+      end
+    end
+  end
 end
