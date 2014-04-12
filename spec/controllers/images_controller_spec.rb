a93f328673daaf354a611d3f779d294f101c5daa
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-04-12 20:06:59 +0800

    Adds Images Controller Spec

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -68,21 +68,61 @@ describe ImagesController do
     context "with media keywords" do
       before :each do
         @media_keyword = FactoryGirl.create :media_keyword
-      end
-
-      it "calls find_all_by_id for keyword" do
         controller.instance_variable_set(:@media_keywords, [@media_keyword])
         controller.get_keywords
+      end
+
+      it "assigns @keywords to Keyword collection" do
         expect(controller.instance_variable_get(:@keywords)).to eq Keyword.scoped
       end
     end
 
     context "without media keywords" do
-      it "assigns @keywords" do
+      it "assigns @keywords with an empty array" do
         controller.get_keywords
         expect(controller.instance_variable_get(:@keywords)).to eq []
       end
     end
 
+  end
+
+  describe "#get_tags" do
+    context "with media tags" do
+      before :each do
+        @media_tag = FactoryGirl.create :media_tag
+        controller.instance_variable_set(:@media_tags, [@media_tag])
+        controller.get_tags
+      end
+
+      it "assigns @movies to Movie collection" do
+        expect(controller.instance_variable_get(:@movies)).to eq Movie.scoped
+      end
+
+      it "assigns @people to Person collection" do
+        expect(controller.instance_variable_get(:@people)).to eq Person.scoped
+      end
+
+      it "assigns @companies to Company collection" do
+        expect(controller.instance_variable_get(:@companies)).to eq Company.scoped
+      end
+    end
+
+    context "without media tags" do
+      before :each do
+        controller.get_tags
+      end
+
+      it "assigns @movies to empty array" do
+        expect(controller.instance_variable_get(:@movies)).to eq []
+      end
+
+      it "assigns @people to empty array" do
+        expect(controller.instance_variable_get(:@people)).to eq []
+      end
+
+      it "assigns @companies to empty array" do
+        expect(controller.instance_variable_get(:@companies)).to eq []
+      end
+    end
   end
 end
