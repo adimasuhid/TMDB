8751c8d92f280d4fb6dc1294ebbea5cdff599989
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-04-12 18:42:08 +0800

    Adds Genre Controllers Specs

--- spec/controllers/genres_controller_spec.rb
+++ spec/controllers/genres_controller_spec.rb
@@ -21,12 +21,12 @@ describe GenresController do
         genre.save!
       end
 
-      it "assigns @genre as a collection" do
+      it "assigns @genres as a collection" do
         get :index
         expect(assigns(:genres)).to be_an_instance_of ActiveRecord::Relation
       end
 
-      it "assigns @genre with an instance of Genre" do
+      it "assigns @genres with an instance of Genre" do
         get :index
         expect(assigns(:genres).sample).to be_an_instance_of Genre
       end
@@ -42,6 +42,55 @@ describe GenresController do
           .and_return(MovieGenre.scoped)
 
         get :index
+      end
+    end
+  end
+
+  describe "#show" do
+    context "with a genres cache" do
+      before :each do
+        genre.save
+        genre.reload
+      end
+
+      it "gets genres from cache" do
+        expect_any_instance_of(Memcached).to receive(:get).with("genre_show_#{genre.id}")
+        get :show, id: genre
+      end
+    end
+
+    context "without a genres cache" do
+      context "with an existing genre" do
+        before :each do
+          genre.approved = true
+          genre.save!
+          genre.reload
+        end
+
+        it "assigns @genre with an instance of Genre" do
+          get :show, id: genre
+          expect(assigns(:genre)).to be_an_instance_of Genre
+        end
+
+        it "responds with a 200 status" do
+          get :show, id: genre
+          expect(response.status).to eq 200
+        end
+
+        it "looks for movie genres for genre" do
+          expect_any_instance_of(Genre).to receive(:movie_genres)
+            .at_least(:once)
+            .and_return(MovieGenre.scoped)
+
+          get :show, id: genre
+        end
+
+      end
+
+      context "without a genre" do
+        it "assigns @genre to be blank" do
+          expect(assigns(:genre)).to be_nil
+        end
       end
     end
   end
