7aa3ef0138c1e307f997a09e97a884242d2a2ba5
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-04-12 22:23:50 +0800

    ListsController#channels

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -20,7 +20,7 @@ describe ListsController do
     context "without a cache" do
       it "assigns @lists" do
         get :index
-        expect(controller.instance_variable_get(:@lists)).to be_an_instance_of ActiveRecord::Relation
+        expect(controller.instance_variable_get(:@lists)).to_not be_nil
       end
 
       it "responds with a 200 status" do
@@ -28,6 +28,72 @@ describe ListsController do
         expect(response.status).to eq 200
       end
     end
+  end
+
+  describe "#galleries" do
+    it "assigns @index_title" do
+      get :galleries
+      expect(controller.instance_variable_get(:@index_title)).to eq("Galleries")
+    end
+
+    context "with a cache" do
+      it "assigns @lists" do
+        expect_any_instance_of(Memcached).to receive(:get).with("static_galleries").and_return("test")
+        get :galleries
+        expect(controller.instance_variable_get(:@lists)).to eq("test")
+      end
+
+      it "responds with a 200 status" do
+        get :galleries
+        expect(response.status).to eq 200
+      end
+    end
+
+    context "without a cache" do
+      it "assigns @lists" do
+        get :galleries
+        expect(controller.instance_variable_get(:@lists)).to_not be_nil
+      end
+
+      it "responds with a 200 status" do
+        get :galleries
+        expect(response.status).to eq 200
+      end
+    end
+
+  end
+
+  describe "#channels" do
+    it "assigns @index_title" do
+      get :channels
+      expect(controller.instance_variable_get(:@index_title)).to eq("Channels")
+    end
+
+    context "with a cache" do
+      it "assigns @lists" do
+        expect_any_instance_of(Memcached).to receive(:get).with("static_channels").and_return("test")
+        get :channels
+        expect(controller.instance_variable_get(:@lists)).to eq("test")
+      end
+
+      it "responds with a 200 status" do
+        get :channels
+        expect(response.status).to eq 200
+      end
+    end
+
+    context "without a cache" do
+      it "assigns @lists" do
+        get :channels
+        expect(controller.instance_variable_get(:@lists)).to_not be_nil
+      end
+
+      it "responds with a 200 status" do
+        get :galleries
+        expect(response.status).to eq 200
+      end
+    end
+
   end
 
 end
