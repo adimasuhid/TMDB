ef8c3661b3ad999cf01273ca62beece12a8a01df
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-07 09:01:14 +0800

    Adds Search Controller Spec

--- spec/requests/api/v1/search_controller_spec.rb
+++ spec/requests/api/v1/search_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Api::V1::SearchController do
-  pending
+  let!(:movie) { create :movie, approved: true }
+  let!(:person) { create :person, approved: true  }
+
+  describe "#search" do
+    context "with related search term" do
+      it "returns person" do
+        get "/api/v1/search.json", term: "mystring"
+        expect(JSON.parse(response.body)).to include({ "id" => person.id, "value" => person.name, "type" => "Person" })
+      end
+
+      it "returns movie" do
+        get "/api/v1/search.json", term: "mystring"
+        expect(JSON.parse(response.body)).to include({ "id" => movie.id, "value" => movie.title, "type" => "Movie" })
+      end
+    end
+
+    context "with unrelated search terms" do
+      it "returns an empty array" do
+        get "/api/v1/search.json", term: "lalala"
+        expect(JSON.parse(response.body)).to eq([])
+      end
+    end
+  end
 end
