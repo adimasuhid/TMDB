cee2a417e010a708a738a763a9dd923cb690c104
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-08 11:41:07 +0800

    Adds List Tag Create

--- spec/requests/api/v1/lists_tags_controller_spec.rb
+++ spec/requests/api/v1/lists_tags_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Api::V1::ListTagsController do
-  pending
+  describe "#create" do
+    it "returns success status" do
+      post "/api/v1/list_tags", list_tag: { temp_user_id: "2"}, tag_ids: [1,2], tag_types: [1,2]
+      expect(JSON.parse(response.body)).to eq({ "status" => "success" })
+    end
+
+    it "creates a ListTag" do
+      expect do
+        post "/api/v1/list_tags", list_tag: { temp_user_id: "2"}, tag_ids: [1,2], tag_types: [1,2]
+      end.to change(ListTag, :count).by(2)
+    end
+  end
 end
