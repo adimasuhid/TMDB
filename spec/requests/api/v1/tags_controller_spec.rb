73ccd8b4ef922726ac7fa1ff7b26171a23a6dfbc
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-08 13:15:53 +0800

    Adds Tag Index

--- spec/requests/api/v1/tags_controller_spec.rb
+++ spec/requests/api/v1/tags_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Api::V1::TagsController do
-  pending
+  let!(:movie) { create :movie }
+  let!(:person) { create :person }
+  let!(:tag) { create :tag, taggable_type: "Movie", taggable_id: movie.id, person_id: person.id }
+
+  describe "#index" do
+    it "assigns @tags" do
+      get "/api/v1/tags"
+      expect(assigns(:tags)).to include tag
+    end
+
+    it "assigns @movies" do
+      get "/api/v1/tags"
+      expect(assigns(:movies)).to include movie
+    end
+
+    it "assigns @people" do
+      get "/api/v1/tags"
+      expect(assigns(:people)).to include person
+    end
+  end
 end
