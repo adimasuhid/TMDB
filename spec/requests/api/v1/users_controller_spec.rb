1e01b572f878467cceef8e919d90e2fc3904569d
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-08 17:00:53 +0800

    Adds Users Controller Spec

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
-  pending
+  let!(:user) { create :user }
+  let!(:admin_user) { create :user, email: "wat@yahoo.com" }
+
+  describe "#show" do
+    it "assigns user" do
+      get "/api/v1/users/#{user.id}", { moderate: true }
+      expect(assigns(:user)).to eq user
+    end
+  end
+
+  describe "#update" do
+    it "returns a user" do
+      get "/api/v1/users/#{user.id}", { moderate: true }
+      expect(JSON.parse(response.body).keys).to include "user"
+    end
+  end
+
+  describe "#get_current_user" do
+    it "assigns user" do
+      get "/api/v1/users/get_current_user", { moderate: true }
+      expect(assigns(:user)).to eq []
+    end
+  end
+
+  describe "#toggle_active" do
+    before :each do
+      allow_any_instance_of(Api::V1::UsersController).to receive(:current_api_user).and_return(user)
+    end
+
+    it "returns user" do
+      put "/api/v1/users/#{user.id}/toggle_active", { moderate: true, user: { toggle_active: true } }
+      expect(response.body).to eq ""
+    end
+  end
 end
