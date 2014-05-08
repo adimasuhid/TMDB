3ab99e759b41e361561376acc955e22cb8f9521b
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-08 12:46:13 +0800

    Refactors Application Controller Spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,17 +1,31 @@
 require 'spec_helper'
 
 describe ApplicationController do
-  controller do
-    def index
-
-    end
-  end
+  let!(:user) { create :user }
 
   describe "#init_cache" do
     it "assigns @cache to a new Memcached" do
-      expect_any_instance_of(Memcached).to receive(:initialize).with("localhost:11211")
-
       get :index
+      expect(assigns(:cache)).to be_an_instance_of Memcached
+    end
+  end
+
+  describe "#authenticate_admin_user!" do
+    context "user is admin" do
+      it "returns nil" do
+        user.user_type = "admin"
+        user.save
+        controller.stub(:current_user).and_return(user)
+        expect(controller.authenticate_admin_user!).to be_nil
+      end
+    end
+
+    context "user is not admin" do
+      it "calls a redirect to root path" do
+        controller.stub(:current_user).and_return(user)
+        expect(controller).to receive(:redirect_to).at_least :once
+        controller.authenticate_admin_user!
+      end
     end
   end
 end
