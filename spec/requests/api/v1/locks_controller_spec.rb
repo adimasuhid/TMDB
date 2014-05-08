63d879f0a01e5b0ceab52d203ee3eb8a0ec10b4c
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-09 00:34:06 +0800

    Adds Unmark Spec

--- spec/requests/api/v1/locks_controller_spec.rb
+++ spec/requests/api/v1/locks_controller_spec.rb
@@ -36,4 +36,38 @@ describe Api::V1::LocksController do
       end
     end
   end
+
+  describe "#unmark" do
+    context "user is not an admin" do
+      it "renders error" do
+        allow_any_instance_of(Api::V1::LocksController).to receive(:current_api_user).and_return(user)
+        post "/api/v1/locks/unmark"
+        expect(response.body).to eq "Error: current user is not admin or moderator."
+      end
+    end
+
+    context "user is an admin" do
+      before :each do
+        user.user_type="admin"
+        user.save
+        allow_any_instance_of(Api::V1::LocksController).to receive(:current_api_user).and_return(user)
+      end
+
+      context "item is saved" do
+        it "returns movie" do
+          post "/api/v1/locks/unmark", item_type: "Movie", item_id: movie.id
+          expect(JSON.parse(response.body)["id"]).to eq movie.id
+        end
+      end
+
+      context "item is not saved" do
+        it "returns error" do
+          expect_any_instance_of(Movie).to receive(:save).and_return(false)
+          post "/api/v1/locks/unmark", item_type: "Movie", item_id: movie.id
+          expect(response.body).to eq "Error saving item."
+        end
+      end
+    end
+
+  end
 end
