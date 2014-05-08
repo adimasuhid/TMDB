e0841ae1fde01653a8ed784e28a38feb9d8725ad
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-08 13:48:40 +0800

    Adds Tag Search Spec

--- spec/requests/api/v1/tags_controller_spec.rb
+++ spec/requests/api/v1/tags_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Api::V1::TagsController do
   let!(:movie) { create :movie }
   let!(:person) { create :person }
+  let!(:company) { create :company }
   let!(:tag) { create :tag, taggable_type: "Movie", taggable_id: movie.id, person_id: person.id }
 
   describe "#index" do
@@ -36,6 +37,25 @@ describe Api::V1::TagsController do
     it "assigns @people" do
       get "/api/v1/tags/#{tag.id}"
       expect(assigns(:people)).to include person
+    end
+  end
+
+  describe "#search" do
+    it "includes movie" do
+      #movie.original_id = movie.id
+      #movie.save
+      get "/api/v1/tags/search", { term: "mystring" }
+      expect(JSON.parse(response.body)).to include({ "id" => movie.id, "value" => movie.title, "type" => "Movie" })
+    end
+
+    it "includes person" do
+      get "/api/v1/tags/search", { term: "mystring" }
+      expect(JSON.parse(response.body)).to include({ "id" => person.id, "value" => person.name, "type" => "Person" })
+    end
+
+    it "includes commpany" do
+      get "/api/v1/tags/search", { term: "mystring" }
+      expect(JSON.parse(response.body)).to include({ "id" => company.id, "value" => company.company, "type" => "Company" })
     end
   end
 end
