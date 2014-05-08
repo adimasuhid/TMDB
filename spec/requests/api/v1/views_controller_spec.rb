1e9ffdc1cb16b60ad2a55155175220d3d39d5e78
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-08 15:57:28 +0800

    Adds View Controller Spec

--- spec/requests/api/v1/views_controller_spec.rb
+++ spec/requests/api/v1/views_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Api::V1::ViewsController do
-  pending
+  describe "#create" do
+    it "returns nil" do
+      post "/api/v1/views", view: { temp_user_id: 1, viewable_type: "lala", viewable_id: 2}
+      expect(response.body).to eq " "
+    end
+
+    it "creates a view" do
+      expect do
+        post "/api/v1/views", view: { temp_user_id: 1, viewable_type: "lala", viewable_id: 2}
+      end.to change(View, :count).by(1)
+    end
+  end
 end
