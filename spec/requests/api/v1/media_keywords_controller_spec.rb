5fc02306e9c2fc27365a47ad3cbad62ef00dae28
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-08 14:12:42 +0800

    Adds Media Keywords Controller Spec

--- spec/requests/api/v1/media_keywords_controller_spec.rb
+++ spec/requests/api/v1/media_keywords_controller_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
 
 describe Api::V1::MediaKeywordsController do
-  pending
+  let!(:keyword) { create :keyword }
+  let!(:user) { create :user }
+  let!(:media_keyword) { create :media_keyword, keyword_id: keyword.id, user_id: user.id }
+
+  describe "#index" do
+    pending #no output, no template
+  end
+
+  describe "#create" do
+
+    it "returns success" do
+      post "/api/v1/media_keywords", media_keyword: { temp_user_id: 2 }, keyword_ids: [1,2]
+      expect(JSON.parse(response.body)).to eq({ "status" => "success" })
+    end
+
+    it "creates a media keyword" do
+      expect do
+        post "/api/v1/media_keywords", media_keyword: { temp_user_id: 2 }, keyword_ids: [1,2]
+      end.to change(MediaKeyword, :count).by(2)
+
+    end
+  end
+
+  describe "#destroy" do
+    before :each do
+      user.user_type = "admin"
+      user.save
+      allow_any_instance_of(Api::V1::MediaKeywordsController).to receive(:current_api_user).and_return(user)
+    end
+
+    context "successful destroy" do
+      it "returns success" do
+        delete "/api/v1/media_keywords/#{media_keyword.id}"
+        expect(JSON.parse(response.body)).to eq({ "status" => "success" })
+      end
+    end
+
+    context "erroneous destroy" do
+      it "returns error" do
+        allow_any_instance_of(ActiveRecord::Relation).to receive(:destroy_all).and_return(false)
+        delete "/api/v1/media_keywords/#{media_keyword.id}"
+        expect(JSON.parse(response.body)).to eq({ "status" => "error" })
+      end
+    end
+
+  end
 end
