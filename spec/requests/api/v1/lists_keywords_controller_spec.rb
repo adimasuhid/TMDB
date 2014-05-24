44179d0c668efb8042c1fb9740f8d8110bb56a3e
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-24 13:53:06 +0800

    Adds List Keyword Spec

--- spec/requests/api/v1/lists_keywords_controller_spec.rb
+++ spec/requests/api/v1/lists_keywords_controller_spec.rb
@@ -1,5 +1,130 @@
 require 'spec_helper'
 
 describe Api::V1::ListKeywordsController do
-  pending
+  let!(:keyword) { create :keyword }
+  let!(:list_keyword) { create :list_keyword, keyword_id: keyword.id }
+  let!(:user) { create :user, user_type: "admin" }
+
+  describe "#index" do
+    pending "no template"
+  end
+
+  describe "#create" do
+    it "creates list keyword" do
+      expect do
+        post "/api/v1/list_keywords", { list_keyword: { temp_user_id: user.id }, keyword_ids: [keyword.id] }
+      end.to change(ListKeyword, :count).by 1
+    end
+
+    it "returns success" do
+      post "/api/v1/list_keywords", { list_keyword: { temp_user_id: user.id }, keyword_ids: [keyword.id] }
+      expect(JSON.parse response.body).to eq({ "status" => "success" })
+    end
+  end
+
+  describe "#destroy" do
+    context "successful destroy" do
+      it "reduces list keyword" do
+        expect do
+          delete "/api/v1/list_keywords/#{list_keyword.id}",
+            {
+              list_keyword: { temp_user_id: user.id },
+              listable_id: list_keyword.listable_id,
+              listable_type: list_keyword.listable_type,
+              keyword_id: list_keyword.keyword_id,
+              temp_user_id: list_keyword.temp_user_id
+            }
+        end.to change(ListKeyword, :count).by(-1)
+      end
+
+      it "returns success" do
+        delete "/api/v1/list_keywords/#{list_keyword.id}",
+          {
+            list_keyword: { temp_user_id: user.id },
+            listable_id: list_keyword.listable_id,
+            listable_type: list_keyword.listable_type,
+            keyword_id: list_keyword.keyword_id,
+            temp_user_id: list_keyword.temp_user_id
+          }
+
+          expect(JSON.parse response.body).to eq({ "status" => "success" })
+      end
+    end
+
+    context "unsuccessful destroy" do
+      it "returns error" do
+        allow_any_instance_of(ActiveRecord::Relation).to receive(:destroy_all).and_return(false)
+        delete "/api/v1/list_keywords/#{list_keyword.id}",
+          {
+            list_keyword: { temp_user_id: user.id },
+            listable_id: list_keyword.listable_id,
+            listable_type: list_keyword.listable_type,
+            keyword_id: list_keyword.keyword_id,
+            temp_user_id: list_keyword.temp_user_id
+          }
+
+          expect(JSON.parse response.body).to eq({ "status" => "error" })
+      end
+    end
+  end
+
+  describe "#update" do
+    context "current api user is an admin" do
+      before :each do
+        user.update_attributes(user_type: "admin")
+        allow_any_instance_of(Api::V1::ListKeywordsController).to receive(:current_api_user).and_return(user)
+      end
+
+      context "no list keywords" do
+        it "returns an error" do
+          put "/api/v1/list_keywords/#{list_keyword.id}",
+            {
+              list_keyword: { temp_user_id: user.id },
+              listable_id: list_keyword.listable_id,
+              listable_type: list_keyword.listable_type,
+              keyword_id: list_keyword.keyword_id,
+              temp_user_id: list_keyword.temp_user_id
+            }
+
+          expect(JSON.parse response.body).to eq({ "status" => "error" })
+        end
+      end
+
+      context "with list keywords" do
+        context "with successful save" do
+          it "returns success" do
+            put "/api/v1/list_keywords/#{list_keyword.id}",
+              {
+                list_keyword: {
+                  temp_user_id: user.id,
+                  listable_id: list_keyword.listable_id,
+                  listable_type: list_keyword.listable_type,
+                  keyword_id: list_keyword.keyword_id,
+                }
+              }
+
+            expect(JSON.parse response.body).to eq({ "status" => "success" })
+          end
+        end
+
+        context "with erroneous save" do
+          it "returns error" do
+            allow_any_instance_of(ListKeyword).to receive(:save).and_return(false)
+            put "/api/v1/list_keywords/#{list_keyword.id}",
+              {
+                list_keyword: {
+                  temp_user_id: user.id,
+                  listable_id: list_keyword.listable_id,
+                  listable_type: list_keyword.listable_type,
+                  keyword_id: list_keyword.keyword_id,
+                }
+              }
+
+            expect(JSON.parse response.body).to eq({ "status" => "error" })
+          end
+        end
+      end
+    end
+
+  end
 end
