7ed58d310d7d5ee4e9d5521ae95e8adf5fe6d4ea
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-05-24 11:38:23 +0800

    Adds People Controller Spec

--- spec/requests/api/v1/people_controller_spec.rb
+++ spec/requests/api/v1/people_controller_spec.rb
@@ -61,4 +61,71 @@ describe Api::V1::PeopleController do
     end
   end
 
+  describe "#show" do
+    context "with cached content" do
+      it "assigns person" do
+        allow_any_instance_of(Memcached).to receive(:get).and_return(people)
+        get "/api/v1/people/#{people.id}.json"
+        expect(assigns(:person)).to_not be_nil
+      end
+    end
+
+    context "without cached content" do
+      it "assigns person" do
+        get "/api/v1/people/#{approved_people.id}.json"
+        expect(assigns(:person)).to_not be_nil
+      end
+
+      it "assigns all" do
+        get "/api/v1/people/#{approved_people.id}.json"
+        expect(assigns(:all)).to_not be_nil
+      end
+
+      it "assigns current api user" do
+        allow_any_instance_of(Api::V1::PeopleController).to receive(:current_api_user).and_return(user)
+        get "/api/v1/people/#{approved_people.id}.json"
+        expect(assigns(:current_api_user)).to_not be_nil
+      end
+    end
+  end
+
+  describe "#my_person" do
+    it "assigns person" do
+      get "/api/v1/people/#{approved_people.id}/my_person.json"
+      expect(assigns(:person)).to_not be_nil
+    end
+
+    it "assigns original person" do
+      get "/api/v1/people/#{approved_people.id}/my_person.json"
+      expect(assigns(:original_person)).to_not be_nil
+    end
+
+    it "assigns all" do
+      get "/api/v1/people/#{approved_people.id}/my_person.json"
+      expect(assigns(:all)).to_not be_nil
+    end
+
+    it "assigns current api user" do
+      allow_any_instance_of(Api::V1::PeopleController).to receive(:current_api_user).and_return(user)
+      get "/api/v1/people/#{approved_people.id}/my_person.json"
+      expect(assigns(:current_api_user)).to_not be_nil
+    end
+
+    it "renders my person" do
+      get "/api/v1/people/#{approved_people.id}/my_person.json"
+      expect(response).to render_template "my_person"
+    end
+  end
+
+  describe "#create" do
+    pending "no template"
+  end
+
+  describe "#search" do
+    it "renders json" do
+      get "/api/v1/people/search", { term: people.name }
+      expect(JSON.parse(response.body)).to include({ "label" => people.name, "value" => people.name, "id" => people.id })
+    end
+  end
+
 end
